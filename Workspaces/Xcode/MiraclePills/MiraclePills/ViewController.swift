a4f0182dd844965e95fa39f956c6a13586f4c3e4
razor08 <razor.sinha.08@gmail.com>
2017-04-22 19:34:06 +0530

    Picker View Added

--- Workspaces/Xcode/MiraclePills/MiraclePills/ViewController.swift
+++ Workspaces/Xcode/MiraclePills/MiraclePills/ViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var statePickerButton: UIButton!
+    
+    
+    let states = ["Arkansas", "Alabama", "New York", "California", "Washington", "New Jersey", "Minnesota", "Ohio"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
 //        self.view.backgroundColor = UIColor.purple
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -21,6 +32,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateButtonPressed(_ sender: Any) {
+        
+        statePicker.isHidden = false
+        
+    }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerButton.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 }
 
