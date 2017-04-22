0a334722a0870fa346288bb5d1dbd2d65d973b4d
razor08 <razor.sinha.08@gmail.com>
2017-04-22 16:26:31 +0530

    First piece of UI Added

---
+++ Workspaces/Xcode/MiraclePills/MiraclePills/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MiraclePills
+//
+//  Created by Jay Sinha on 22/04/2017.
+//  Copyright © 2017 Jay Sinha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.view.backgroundColor = UIColor.purple
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
